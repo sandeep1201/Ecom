95d2c1431e646e73fa08d18f07eab2fe9550f3cc
Sandeep Reddy <sra29470@gmail.com>
2019-02-17 16:47:44 -0600

    Updates

---
+++ precache-manifest.dd3d902d51e13dd261e682ffe28ebfbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/sandeep1201/Ecom/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "f281e4896f71b8e4912a",
+    "url": "/sandeep1201/Ecom/static/js/runtime~main.f281e489.js"
+  },
+  {
+    "revision": "cee0d969f68f58b3fb6f",
+    "url": "/sandeep1201/Ecom/static/js/main.cee0d969.chunk.js"
+  },
+  {
+    "revision": "8d7f8479b79b3faac32c",
+    "url": "/sandeep1201/Ecom/static/js/1.8d7f8479.chunk.js"
+  },
+  {
+    "revision": "cee0d969f68f58b3fb6f",
+    "url": "/sandeep1201/Ecom/static/css/main.faff8312.chunk.css"
+  },
+  {
+    "revision": "8d7f8479b79b3faac32c",
+    "url": "/sandeep1201/Ecom/static/css/1.3e421d03.chunk.css"
+  },
+  {
+    "revision": "755874a9313cd98c56e64f61af5cba12",
+    "url": "/sandeep1201/Ecom/index.html"
+  }
+];
